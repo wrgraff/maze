1d77fef3cb91572e3c531db16a98755073a25269
Artur Trifonov <wrgraff@gmail.com>
2020-11-21 02:50:49 +0300

    init maze

---
+++ index.js
@@ -0,0 +1,58 @@
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        wireframes: false,
+        width,
+        height
+    }
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+
+// Walls
+const walls = [
+    Bodies.rectangle(400, 0, width, 40, {
+        isStatic: true
+    }),
+    Bodies.rectangle(width, 300, 40, height, {
+        isStatic: true
+    }),
+    Bodies.rectangle(400, height, width, 40, {
+        isStatic: true
+    }),
+    Bodies.rectangle(0, 300, 40, height, {
+        isStatic: true
+    }),
+];
+
+World.add(world, walls);
+
+for (let i = 0; i < 50; i++) {
+    if (Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(
+            Math.random() * width,
+            Math.random() * height,
+            50,
+            50
+        ));
+    } else {
+        World.add(world, Bodies.circle(
+            Math.random() * width,
+            Math.random() * height,
+            32
+        ));
+    }
+};
